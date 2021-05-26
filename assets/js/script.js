a440ecf4b95f731f34acf76706bdf6cfd454a983
JaimeVilches87 <jaimevilches87@gmail.com>
2021-05-25 22:20:47 -0400

    added intital functions to get zipcode and city 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
 let APIKey = "166a433c57516f51dfab1f7edaed8413";
+
+
+function weatherZip() {
+
+var queryUrl = "api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={API key}";
+
+
+}
+
+
+function weatherCity() {
+var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + ",us&appid=" + APIKey;
+
+}
